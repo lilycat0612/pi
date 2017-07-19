9d9815c272b31096af562de54079921f0458d6ec
lilycat0612 <27803532@qq.com>
2017-07-19 14:51:08 +0800

    add Deluge.py

---
+++ Deluge.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+__author__ = 'srv'
+#!/usr/bin/python
+
+from selenium import webdriver
+import time
+import os
+
+
+dir=os.path.dirname(__file__)
+chrome_driver_path=dir+"\chromedriver.exe"
+browser=webdriver.Chrome(chrome_driver_path)
+browser.get("http://192.168.1.104:8112/")
